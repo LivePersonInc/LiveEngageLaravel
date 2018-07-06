a2c8629ebfb69096b91ffa172779e9fc9c6270a3
Rob Lester <rlester@liveperson.com>
2018-07-06 09:47:33 -0400

    Add attribute mutator for endtime.

--- src/Models/MessagingInfo.php
+++ src/Models/MessagingInfo.php
@@ -21,6 +21,11 @@ class MessagingInfo extends Model
 	{
 		return new Carbon($this->attributes['startTime']);
 	}
+	
+	public function getEndTimeAttribute()
+	{
+		return new Carbon($this->attributes['endTime']);
+	}
 
 	public function getMinutesAttribute()
 	{
