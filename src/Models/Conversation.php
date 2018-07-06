ee313eff4d407e5747c3815a8f8a2024986a827c
Rob Lester <rlester@liveperson.com>
2018-07-06 17:13:37 -0400

    Reduce crap score.

--- src/Models/Conversation.php
+++ src/Models/Conversation.php
@@ -29,14 +29,27 @@ class Conversation extends Model
 	
 	public function __construct(array $item)
 	{
-		$item['info'] = isset($item['info']) ? new MessagingInfo((array) $item['info']) : new MessagingInfo();
-		$item['visitorInfo'] = isset($item['visitorInfo']) ? new Visitor((array) $item['visitorInfo']) : new Visitor();
-		$item['campaign'] = isset($item['campaign']) ? new Campaign((array) $item['campaign']) : new Campaign();
-		$item['transfers'] = new Transfers(isset($item['transfers']) ? $item['transfers'] : []);
-		$item['agentParticipants'] = new AgentParticipants(isset($item['agentParticipants']) ? $item['agentParticipants'] : []);
-		$item['consumerParticipants'] = new ConsumerParticipants(isset($item['consumerParticipants']) ? $item['consumerParticipants'] : []);
-		$item['messageRecords'] = new Transcript(isset($item['messageRecords']) ? $item['messageRecords'] : [], $item['agentParticipants']);
-		$item['sdes'] = new SDEs(isset($item['sdes']->events) ? $item['sdes']->events : []);
+		$init = [
+			'info'					=> [],
+			'visitorInfo'			=> [],
+			'campaign'				=> [],
+			'transfers'				=> [],
+			'agentParticipants'		=> [],
+			'consumerParticipants'	=> [],
+			'messageRecords'		=> [],
+			'sdes'					=> new SDE()
+		];
+		
+		$item = array_merge($init, $item);
+		
+		$item['info'] = new MessagingInfo((array) $item['info']);
+		$item['visitorInfo'] = new Visitor((array) $item['visitorInfo']);
+		$item['campaign'] = new Campaign((array) $item['campaign']);
+		$item['transfers'] = new Transfers($item['transfers']);
+		$item['agentParticipants'] = new AgentParticipants($item['agentParticipants']);
+		$item['consumerParticipants'] = new ConsumerParticipants($item['consumerParticipants']);
+		$item['messageRecords'] = new Transcript($item['messageRecords'], $item['agentParticipants']);
+		$item['sdes'] = new SDEs($item['sdes']->events ?: []);
 		
 		parent::__construct($item);
 	}
