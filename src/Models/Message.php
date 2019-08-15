c17fd9f3558cd011d9fdd71446a4c6fcee1da3a5
Robert Lester <rlester@liveperson.com>
2019-08-15 12:07:25 -0400

    ..

--- src/Models/Message.php
+++ src/Models/Message.php
@@ -20,18 +20,18 @@ class Message extends Model
 		'time',
 		'rich_content'
 	];
-	
+
 	public function getTextAttribute()
 	{
 		if ($this->type == 'TEXT_PLAIN') {
 			return $this->messageData->msg->text;
 		} elseif ($this->type == 'RICH_CONTENT') {
-			return 'RICH_CONTENT'; // @codeCoverageIgnore
+			return $this->parseRichContent(); // @codeCoverageIgnore
 		} else {
 			return isset($this->attributes['text']) ? $this->attributes['text'] : '';
 		}
 	}
-	
+
 	public function getRichContentAttribute()
 	{
 		if ($this->type == 'RICH_CONTENT') {
@@ -54,5 +54,26 @@ class Message extends Model
 	public function __toString()
 	{
 		return $this->text;
+	}
+
+	private function parseRichContent()
+	{
+		$content = json_decode($this->messageData->richContent->content);
+
+		$type = $content->type;
+		$tag = $content->tag;
+		$elements = $content->elements;
+
+		$output = '';
+
+		if ($tag == 'button') {
+			foreach ($elements as $button) {
+				if ($button->type != 'button') continue;
+				$output .= "[{$button->title}]";
+			}
+			return $output;
+		} else {
+			return 'RICH_CONTENT';
+		}
 	}
 }
